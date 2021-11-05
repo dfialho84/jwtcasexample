1dba7f3c9682f63d566d63d04930a12c8e997eb9
Diego Fialho Rodrigues <dfialho84@gmail.com>
2021-11-05 09:41:05 -0300

    Commit inicial

---
+++ src/main/java/br/com/sekka/jwtcasexample/JwtcasexampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.sekka.jwtcasexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtcasexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtcasexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sekka/jwtcasexample/JwtcasexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sekka.jwtcasexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtcasexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
